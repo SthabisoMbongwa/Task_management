b869eaf36a70ff70fed76edd6bbf0816cdc2cc04
Sthabiso <sthabiso@aims.ac.za>
2023-11-01 09:56:24 +0200

    Created a Navbar component

--- task/src/App.js
+++ task/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>This is my task management project</h1>
     </div>
   );
---
+++ task/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = function (){
+    return(
+        <nav>
+            <h1>This is my nav</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
