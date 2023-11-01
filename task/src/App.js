0b00521dc19ba2c99691ab9719ad5e563059ba13
Sthabiso <sthabiso@aims.ac.za>
2023-11-01 10:35:29 +0200

    Created the body section component

--- task/src/App.js
+++ task/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import BodySection from './components/Bodysection';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>This is my task management project</h1>
+      <BodySection />
     </div>
   );
 }
---
+++ task/src/components/Bodysection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BodySection = function (){
+    return(
+        <div>
+            <h1>This is the body section.</h1>
+        </div>
+    )
+}
+
+export default BodySection;
