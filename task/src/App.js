be10fcabd03117c1afb82ef2366bc730a08f5e4e
Sthabiso <sthabiso@aims.ac.za>
2023-11-08 21:50:42 +0200

    Added Routing to navigate around

--- task/src/App.js
+++ task/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import LandingPage from './components/LandingPage';
 import VerifyPage from './components/VerifyPage';
-
+import {Routes, Route} from 'react-router-dom';
 
 
 function App() {
   
   return (
     <div className="App">
-      <LandingPage />
-      <VerifyPage />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='verifypage' element={<VerifyPage />} />
+      </Routes>
+      {/* <LandingPage />
+      <VerifyPage /> */}
     </div>
   );
 }
