8124ea7cf8b691731d7405e0b6e47f7664b10c27
Sthabiso <sthabiso@aims.ac.za>
2023-11-16 19:32:45 +0200

    Added link to the logo so that it is clickable

--- task/src/components/Navbar.jsx
+++ task/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import LoginPage from './LoginPage';
 import { Link } from 'react-router-dom';
+import LandingPage from './LandingPage';
 
 const Navbar = function (){
     return(
 
         <nav className='nav'>
-            <h1>P</h1>
+            <h1><Link className="sub" to='/' element={<LandingPage />}> P </Link></h1>
             <ul>
                 <li><button className='btn'><Link className="sub" to='/login' element={<LoginPage />}>Log In</Link></button></li>
             </ul>
