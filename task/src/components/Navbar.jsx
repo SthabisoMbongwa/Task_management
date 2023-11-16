04d0ee0592aa807032341cf660ec50de470e6762
Sthabiso <sthabiso@aims.ac.za>
2023-11-16 09:47:51 +0200

    Added Link on the login button

--- task/src/components/Navbar.jsx
+++ task/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import LoginPage from './LoginPage';
+import { Link } from 'react-router-dom';
 
 const Navbar = function (){
     return(
@@ -6,7 +8,7 @@ const Navbar = function (){
         <nav className='nav'>
             <h1>P</h1>
             <ul>
-                <li><button>Log In</button></li>
+                <li><button className='btn'><Link className="sub" to='/login' element={<LoginPage />}>Log In</Link></button></li>
             </ul>
 
         </nav>
